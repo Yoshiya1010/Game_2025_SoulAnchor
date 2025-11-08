c07148517224e9abcc58301d93773e9236cf5167
Yoshiya1010 <naganoyoshiya721@gmail.com>
2025-11-08 11:01:05 +0900

    フィルターの追加とスプライトアニメーションの修正

---
+++ GM_1004_SoulAnchor/spriteAnimator2Dcpp.cpp
@@ -0,0 +1,119 @@
+#include"spriteAnimator2D.h"
+
+void SpriteAnimator2D::Init(float x, float y, float w, float h, const char* FileName, int cols, int rows)
+{
+    m_X = x; m_Y = y; m_W = w; m_H = h;
+    m_Cols = cols; m_Rows = rows;
+
+    VERTEX_3D v[4] =
+    {
+        {{x,     y,     0}, {}, {1,1,1,1}, {0,0}},
+        {{x + w,   y,     0}, {}, {1,1,1,1}, {1,0}},
+        {{x,     y + h,   0}, {}, {1,1,1,1}, {0,1}},
+        {{x + w,   y + h,   0}, {}, {1,1,1,1}, {1,1}},
+    };
+
+    D3D11_BUFFER_DESC bd{};
+    bd.Usage = D3D11_USAGE_DYNAMIC;
+    bd.ByteWidth = sizeof(VERTEX_3D) * 4;
+    bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+    bd.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+
+    D3D11_SUBRESOURCE_DATA sd{};
+    sd.pSysMem = v;
+
+    Renderer::GetDevice()->CreateBuffer(&bd, &sd, &m_VertexBuffer);
+
+    m_Texture = TextureManager::Load(FileName);
+
+    Renderer::CreateVertexShader(&m_VertexShader, &m_VertexLayout, "shader\\unlitTextureVS.cso");
+    Renderer::CreatePixelShader(&m_PixelShader, "shader\\unlitTexturePS.cso");
+}
+
+void SpriteAnimator2D::Update()
+{
+
+    if (!m_Playing) return; //プレイ中じゃなかったら帰す
+
+    m_Timer += 1.0f / 60.0f;
+
+    if (m_Timer >= m_FrameTime)
+    {
+        m_Timer = 0;
+        m_Frame++;
+
+        if (m_Frame >= m_Cols * m_Rows)
+            m_Frame = 0;//ループさせる
+    }
+
+}
+
+void SpriteAnimator2D::Draw()
+{
+    Renderer::SetWorldViewProjection2D();
+    Renderer::SetDepthEnable(false);
+
+    //頂点を求める
+    float cx = m_X;
+    float cy = m_Y;
+    float w = m_W;
+    float h = m_H;
+
+    float x0 = cx - w / 2.0f;
+    float y0 = cy - h / 2.0f;
+
+    VERTEX_3D v[4] =
+    {
+        {{x0,     y0,     0}, {}, {1,1,1,1}, {}},
+        {{x0 + w, y0,     0}, {}, {1,1,1,1}, {}},
+        {{x0,     y0 + h, 0}, {}, {1,1,1,1}, {}},
+        {{x0 + w, y0 + h, 0}, {}, {1,1,1,1}, {}},
+    };
+
+    //UVを求める
+    int col = m_Frame % m_Cols;
+    int row = m_Frame / m_Cols;
+    float u0 = (float)col / m_Cols;
+    float v0 = (float)row / m_Rows;
+    float u1 = (float)(col + 1) / m_Cols;
+    float v1 = (float)(row + 1) / m_Rows;
+
+    v[0].TexCoord = { u0, v0 };
+    v[1].TexCoord = { u1, v0 };
+    v[2].TexCoord = { u0, v1 };
+    v[3].TexCoord = { u1, v1 };
+
+    //頂点更新
+    D3D11_MAPPED_SUBRESOURCE mp;
+    Renderer::GetDeviceContext()->Map(m_VertexBuffer, 0, D3D11_MAP_WRITE_DISCARD, 0, &mp);
+    memcpy(mp.pData, v, sizeof(v));
+    Renderer::GetDeviceContext()->Unmap(m_VertexBuffer, 0);
+
+
+    Renderer::GetDeviceContext()->IASetInputLayout(m_VertexLayout);
+    Renderer::GetDeviceContext()->VSSetShader(m_VertexShader, NULL, 0);
+    Renderer::GetDeviceContext()->PSSetShader(m_PixelShader, NULL, 0);
+
+    MATERIAL mat{};
+    mat.Diffuse = { 1,1,1,1 };
+    mat.TextureEnable = true;
+    Renderer::SetMaterial(mat);
+
+    UINT stride = sizeof(VERTEX_3D);
+    UINT offset = 0;
+    Renderer::GetDeviceContext()->IASetVertexBuffers(0, 1, &m_VertexBuffer, &stride, &offset);
+    Renderer::GetDeviceContext()->PSSetShaderResources(0, 1, &m_Texture);
+    Renderer::GetDeviceContext()->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+    Renderer::GetDeviceContext()->Draw(4, 0);
+
+    Renderer::SetDepthEnable(true);
+}
+
+
+void SpriteAnimator2D::Uninit()
+{
+    if (m_VertexBuffer) m_VertexBuffer->Release();
+    if (m_VertexLayout) m_VertexLayout->Release();
+    if (m_VertexShader) m_VertexShader->Release();
+    if (m_PixelShader)  m_PixelShader->Release();
+}
--- GM_1004_SoulAnchor/title.cpp
+++ GM_1004_SoulAnchor/title.cpp
@@ -1,15 +1,22 @@
 #include "main.h"
 #include "manager.h"
 #include "renderer.h"
-
 #include "input.h"
 #include "title.h"
 #include "game.h"
 #include "Polygon.h"
+#include"spriteAnimator2D.h"
 
 void Title::Init()
 {
-	AddGameObject<Polygon2D>(UI)->Init(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT, "asset\\texture\\Title_BG.png");
+
+	AddGameObject<Polygon2D>(UI)->Init(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT, "asset\\texture\\TitleScene\\title_background.jpg");
+
+	auto logo = AddGameObject<SpriteAnimator2D>(UI);
+	logo->Init(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 600, 300,
+		"asset\\texture\\TitleScene\\title_logo_text_loop.png",
+		6, 6);
+	logo->SetFrameSpeed(0.08f);
 }
 
 void Title::Update()
